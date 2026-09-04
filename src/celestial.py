82a6a6594481719916e6a96f29d5b1b698d23122
Allison Bianchi <allisonb@pixar.com>
2026-09-03 23:59:00 -0700

    Two-frame anim on walk

--- src/celestial.py
+++ src/celestial.py
@@ -7,8 +7,14 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+elapsed = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+frames = [
+    pygame.image.load("../assets/maddy/maddy-walk-tmp-3.png").convert_alpha(),
+    pygame.image.load("../assets/maddy/maddy-walk-tmp-4.png").convert_alpha()
+]
 
 while running:
     # poll for events
@@ -18,23 +24,30 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill((0, 0, 0))
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    frameIdx = int(elapsed / 0.2) % len(frames)
+    screen.blit(frames[frameIdx], player_pos)
 
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_q] or keys[pygame.K_ESCAPE]:
         running = False
 
+    moving = False
+
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= 150 * dt
+        moving = True
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += 150 * dt
+        moving = True
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= 150 * dt
+        moving = True
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += 150 * dt
+        moving = True
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -43,5 +56,6 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
+    elapsed = elapsed + dt if moving else 0
 
 pygame.quit()
